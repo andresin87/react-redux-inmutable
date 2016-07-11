219f84889a9b1913e5dc94765d15f711d52f5476
Andrés Lucas <alucas@userzoom.com>
2016-07-11 15:13:01 +0200

    WORKING Provider added

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import { TodoList } from './components';
+import { Provider } from 'react-redux';
 import reducer from './reducer';
+import { TodoList } from './containers';;
 
 const store = createStore(reducer);
 
